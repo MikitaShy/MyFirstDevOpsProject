e6096db099433a16f111841e1f1204e9467b29ef
Mikita Shylau <mish0421@netcracker.com>
2021-04-08 20:16:11 +0300

    MyProject

---
+++ launching_programs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+usermod vagrant -aG root
+sudo apt-get clean all
+sudo apt update -y 
+sudo apt-get install -y git
+sudo apt install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt update -y
+sudo apt install ansible -y
+sudo echo "Centos  ansible_host=10.0.0.120  ansible_user=vagrant   ansible_password=vagrant" | sudo tee -a /etc/ansible/hosts
+echo "+++++++++++++++++++++++++++++++Installed"
