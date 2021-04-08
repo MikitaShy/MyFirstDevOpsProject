966f9c645979fea6e1a8e43572617984368771a2
Mikita Shylau <mish0421@netcracker.com>
2021-04-08 21:25:52 +0300

    editnginx

--- MyScript.sh
+++ MyScript.sh
@@ -14,7 +14,7 @@ echo "====Create 2 VM===="
 vagrant up
 sleep 3
 echo "====Ansible Install Jenkins in Centos===="
-vagrant ssh Ubuntu -- -t 'mkdir ~/ansible/; cd ~/ansible/; git init; git clone https://github.com/MikitaShy/MyVagrant-Ansible.git; cd MyVagrant-Ansible/; sudo ansible-playbook jenkins.yml; sudo ansible-playbook playbook_centos_install_docker.yml'
+vagrant ssh Ubuntu -- -t 'mkdir ~/ansible/; cd ~/ansible/; git init; git clone https://github.com/MikitaShy/MyFirstDevOpsProject.git; cd MyVagrant-Ansible/; sudo ansible-playbook jenkins.yml; sudo ansible-playbook playbook_centos_install_docker.yml'
 sleep 3
 echo "====Install Docker and run MyStatic_Site===="
 vagrant ssh Centos -- -t 'mkdir ~/docker/; cd docker/; git init; git clone https://github.com/MikitaShy/Docker_MyStatic_Site.git ;  sudo docker build -t m.shylau/nginx ~/docker/Docker_MyStatic_Site/static-site/ ; sudo docker run -dit -p 8888:80 m.shylau/nginx'
