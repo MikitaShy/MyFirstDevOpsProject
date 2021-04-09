96f8433cc03fb069b0070bb2868f7daf1c81329e
Mikita Shylau <mish0421@netcracker.com>
2021-04-09 12:29:37 +0300

    mish0421

--- MyScript.sh
+++ MyScript.sh
@@ -8,11 +8,11 @@ echo "====Create 2 VM===="
 vagrant up
 echo "====Ansible Install Jenkins in Centos===="
 vagrant ssh Ubuntu -- -t 'mkdir ~/ansible/; cd ~/ansible/; git init; git clone https://github.com/MikitaShy/MyFirstDevOpsProject.git; 
-cd MyVagrant-Ansible/; sudo ansible-playbook jenkins.yml; sudo ansible-playbook playbook_centos_install_docker.yml'
+cd MyFirstDevOpsProject./MyS/; sudo ansible-playbook jenkins.yml; sudo ansible-playbook playbook_centos_install_docker.yml'
 sleep 3
 echo "====Install Docker and run MyStatic_Site===="
 vagrant ssh Centos -- -t 'mkdir ~/docker/; cd docker/; git init; git clone https://github.com/MikitaShy/Docker_MyStatic_Site.git; 
-sudo docker build -t mish0421/nginx ~/docker/Docker_MyStatic_Site/static-site/ ; sudo docker run -dit -p 8888:80 m.shylau/nginx'
+sudo docker build -t mish0421/nginx ~/docker/Docker_MyStatic_Site/static-site/ ; sudo docker run -dit -p 8888:80 mish0421/nginx'
 elif [[ ( $edit == "d" ) ]]; then
 vagrant destroy
 else
